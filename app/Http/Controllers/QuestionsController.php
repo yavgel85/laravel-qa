0af3de10ae57540bf9139583e493d8b0993b9dd1
Dmytry <vuyko@ukr.net>
2018-12-05 16:43:58 +0200

    Extend the form functionality to be able to update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -67,19 +67,24 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view('questions.edit', compact('question'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  AskQuestionRequest  $request
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+
+        return redirect()
+            ->route('questions.index')
+            ->with('success', 'Your question has been updated')
+        ;
     }
 
     /**
---
+++ resources/views/questions/_form.blade.php
@@ -0,0 +1,36 @@
+@csrf
+
+<div class="form-group">
+    <label for="question-title">Question Title</label>
+    <input
+        id="question-title"
+        class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}"
+        type="text"
+        name="title"
+        value="{{ old('title', $question->title) }}"
+    >
+
+    @if($errors->has('title'))
+        <div class="invalid-feedback">
+            <strong>{{ $errors->first('title') }}</strong>
+        </div>
+    @endif
+</div>
+<div class="form-group">
+    <label for="question-body">Explain your Question</label>
+    <textarea
+        name="body"
+        rows="10"
+        id="question-body"
+        class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body', $question->body) }}
+    </textarea>
+
+    @if($errors->has('body'))
+        <div class="invalid-feedback">
+            <strong>{{ $errors->first('body') }}</strong>
+        </div>
+    @endif
+</div>
+<div class="form-group">
+    <button type="submit" class="btn btn-outline-primary btn-lg">{{ $buttonText }}</button>
+</div>
