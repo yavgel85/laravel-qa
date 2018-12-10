f696daa84b820293749e5116aecb13be37f8ffae
Dmytry <vuyko@ukr.net>
2018-12-10 11:25:44 +0200

    display answers for question with avatar

--- app/User.php
+++ app/User.php
@@ -38,6 +38,15 @@ class User extends Authenticatable
         return '#';
     }
 
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 32;
+
+        return 'https://www.gravatar.com/avatar/' . md5( strtolower( trim( $email ) ) ) . '?s=' . $size;
+
+    }
+
     public function answers()
     {
         return $this->hasMany(Answer::class);
