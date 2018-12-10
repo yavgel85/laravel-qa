5efa00fadd2059a19f004654be42067ea6b64090
Dmytry <vuyko@ukr.net>
2018-12-10 15:57:49 +0200

    display answers for question

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('questions', 'QuestionsController')->except('show');
+Route::resource('questions.answers', 'AnswersController')->only(['store', 'edit', 'update', 'destroy']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
