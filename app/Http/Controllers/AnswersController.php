09238b195ffc44c22786202f87bab852a6aa4b36
Dmytry <vuyko@ukr.net>
2018-12-10 18:14:52 +0200

    Implement delete functionality for answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -62,11 +62,15 @@ class AnswersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Answer  $answer
-     * @return \Illuminate\Http\Response
+     * @param Question $question
+     * @param  \App\Answer $answer
      */
-    public function destroy(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('delete', $answer);
+
+        $answer->delete();
+
+        return back()->with('success', 'Your answer has been removed');
     }
 }
